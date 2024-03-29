31de6f49c81d4ee837a0cf4733be4b3a9ed3d0f2
Shrey(personal) <shreypersonal@shreyashs-Laptop.local>
2024-03-29 11:53:34 +0530

    Added google signin and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 "use client"
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthContextProvider } from "./context/authContaxt";
 import Navbar from "./component/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "Baawaa",
-//   description: "Created By Kids"
-// };
 
 export default function RootLayout({
   children,
